201040bde3d3a33bf86a13cea2bb364c28b965c1
alvaroherediasp-hash <alvaroherediasp-hash@users.noreply.github.com>
2026-06-29 23:05:09 -0300

    Mover public/ a raiz para GitHub Pages

--- iniciar.bat
+++ iniciar.bat
@@ -4,11 +4,9 @@ cd /d "%~dp0"
 echo.
 echo ========================================
 echo   SISTEMA DE GESTION DE STOCK
-echo   Iniciando servidor de imagenes...
 echo ========================================
 echo.
 echo Abriendo la aplicacion...
-start "" "http://localhost:3001"
-start "" "public/index.html"
+start "" "index.html"
 node image-server.js
 pause
