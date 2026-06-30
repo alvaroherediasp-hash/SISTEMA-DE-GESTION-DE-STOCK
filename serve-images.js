b257dde3fd500b2d764cef68890b70ac720d681c
alvaroherediasp-hash <alvaroherediasp-hash@users.noreply.github.com>
2026-06-29 22:46:31 -0300

    Remover archivo duplicado

--- serve-images.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
-app.listen(3001, () => console.log('Servidor de imagenes en http://localhost:3001'));
